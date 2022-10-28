925d7573ecea3a6e1465636e475f7c6098e67bd6
Richard Carrion <rcarrion3406@gmail.com>
2022-10-28 11:22:37 -0500

    tree info

--- tree.py
+++ tree.py
@@ -5,29 +5,33 @@ class Tree:
         self.root = None
         self.puzzle = puzzle
         self.solution = None
+        self.leafs = []
+        self.open_nodes = 0
+        self.iter = 0
 
 
     def solve(self) -> None:
         self.root = Node(self.puzzle)
         self.current = self.root
-        i = 0
+        self.leafs.append(self.root)
         while (self.solution is None 
             or self.solution is not self.root.search_leaf_node_lower_or_equal_than(self.solution)):
-            i += 1
-            print(f"Busqueda: {i}")
+            self.iter += 1
+            print(f"Busqueda: {self.iter}")
 
-            leaf_node = self.root.search_first_leaf_node()
-
-            if self.root.exists_leaf_node_lower_than(leaf_node):
-                self.current = self.root.search_leaf_node_lower_than(leaf_node)
-            else:
-                self.current = self.root.search_leaf_node_lower_or_equal_than(leaf_node)
+            leaf_node = self.leafs[0]
+            self.current = self.root.search_leaf_node_lower_or_equal_than(leaf_node)
 
             if self.current:
-
                 self.solution = self.current.generate_children()
+                if self.current.children:
+                    self.leafs.remove(self.current)
+                    self.open_nodes += 1
+                    for child in self.current.children:
+                        self.leafs.append(child)
 
-        print(f"Total de iteraciones: {i} ")
+            self.leafs.sort(key=lambda hoja: hoja.f())
+
 
 
 
